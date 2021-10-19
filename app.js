527206175d3d38c689dd9c9d742b96e38c92b6d3
lukita10 <exoluca@gmail.com>
2021-10-18 21:25:31 -0300

    Primer push

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const path = require("path")
+const PUERTO = 3000
+app.use(express.static('./public'));
+
+
+app.get('/', function(req,res){res.sendFile(path.join(__dirname, './views/home.html'))});
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname, './views/register.html'))
+})
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname, './views/login.html'))
+    
+})
+app.get('/carrito',(req,res)=>{
+    res.sendFile(path.join(__dirname, './views/carrito.html'))
+})
+app.get('/productDetail',(req,res)=>{
+    res.sendFile(path.join(__dirname, './views/productDetail.html'))
+})    
+
+app.listen(PUERTO, () => console.log('Servidor corriendo; PUERTO 3000!!'));
